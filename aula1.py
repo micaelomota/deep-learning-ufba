5182435770455f704b8474758cd8fb92a64731f8
Micael Mota <micaelomota@hotmail.com>
2018-09-14 11:20:28 -0300

    organizando

--- aula1.py
+++ aula1.py
@@ -1,46 +1,23 @@
 import os
 import numpy as np
 import cv2
+from helpers import dataloader
 
-# carrega uma imagem colorida (3 canais, BGR)
 #color = cv2.imread('nome.jpg', cv2.IMREAD_COLOR)
-
-# carrega uma imagem em tons de cinza (1 canal)
 #gray = cv2.imread('nome.jpg', cv2.IMREAD_GRAYSCALE)
-
-# carrega uma imagem como ela é (X canais)
 #img = cv2.imread('data_part1/test/14734.png', cv2.IMREAD_UNCHANGED)
-
 # visualiza imagem até que uma tecla seja pressionada
 #cv2.imshow('image', img)
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
-
 # salva imagem
 #cv2.imwrite('saida.png', img)
 
-test_path = "data_part1/train/"
+data, labels = dataloader.loadData("data_part1/train/")
+td, tl, vd, vl = dataloader.splitValidation(data, labels, 10)
 
-folders = os.listdir(test_path)
+print(tl[0])
 
-height = 77
-width = 71
-num_channels = cv2.IMREAD_COLOR
-num_images = 12
-
-tensor4d = np.empty([num_images, height, width, num_channels], dtype=np.uint8)
-
-for folder in folders: 
-    print ("carregando pasta " + folder)
-    
-    files = os.listdir(test_path + folder)
-    print(str(len(files)) + " arquivos encontrados")
-
-
-
-quit()
-
-
-
-for img in lista:
-    tensor4d.append(cv2.imread(test_path + img, cv2.IMREAD_COLOR))
+cv2.imshow('Imagem', td[0])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ helpers/testDataloader.py
@@ -0,0 +1,13 @@
+import dataloader
+import cv2
+
+data, labels = dataloader.loadData("data_part1/train/")
+
+
+td, tl, vd, vl = dataloader.splitValidation(data, labels, 10)
+
+print(tl[0])
+
+cv2.imshow('Imagem', td[0])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
