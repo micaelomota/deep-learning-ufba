bbc8836a0661d487de601b1b5ba2b17fa470982a
Micael Mota <micael.mota@defensoria.ba.def.br>
2018-10-09 16:38:36 -0300

    inference

--- inference.py
+++ inference.py
@@ -1,15 +1,20 @@
 import numpy as np
 from src import dataloader
+import cv2
 
+w = np.load("models/logisticRegression/w.npy")
+b = np.load("models/logisticRegression/b.npy")
 
-data, names = dataloader.loadTestData('data_part1/test/')
-data = np.reshape(data, (len(data), 77*71))
+#imagePath = ""
 
-w = np.load("w.npy")
-b = np.load("b.npy")
+#img = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
 
-for i in range(len(data)):
-    y_ = np.dot(data[i], w) + b
-    shot = np.argmax(y_)
+def runTests():
+    
+    data, names = dataloader.loadTestData('data_part1/test/')
+    data = np.reshape(data, (len(data), 77*71))
+    for i in range(len(data)):
+        y_ = np.dot(data[i], w) + b
+        shot = np.argmax(y_)
 
-    print("{} {}".format(names[i], shot))
+        print("{} {}".format(names[i], shot))
